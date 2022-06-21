4a3c4468227a5ebe218d2d2288cda4ecb9f39bd9
Diptonil <diptonilr@gmail.com>
2022-06-21 08:34:59 +0530

    things

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,11 +9,18 @@ def user_directory_path(instance, filename):
 
 class Post(models.Model):
 
+    CHOICES = (
+        ('Motivational', 'Motivational'),
+        ('Experience', 'Experience'),
+        ('Opinion-Based', 'Opionion-Based'),
+        ('Uncategorized', 'Uncategorized')
+    )
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=64)
     post = models.TextField()
     date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to=user_directory_path, blank=True)
+    tags = models.CharField(choices=CHOICES, max_length=16, default='Uncategorized')
 
     class Meta:
         ordering = ('-date',)
